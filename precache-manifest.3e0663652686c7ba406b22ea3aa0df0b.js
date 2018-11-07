8188b741e4dc48dac22c9c8092cfbbe193fdc8ba
Stewart Sum <stewartsum@users.noreply.github.com>
2018-11-07 01:51:28 -0700

    Updates

---
+++ precache-manifest.3e0663652686c7ba406b22ea3aa0df0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6d0aa42199878ab9859",
+    "url": "/react-tutorial/static/js/runtime~main.d6d0aa42.js"
+  },
+  {
+    "revision": "95268a1ebb7071315830",
+    "url": "/react-tutorial/static/js/main.95268a1e.chunk.js"
+  },
+  {
+    "revision": "1670935a042b1fdc38db",
+    "url": "/react-tutorial/static/js/1.1670935a.chunk.js"
+  },
+  {
+    "revision": "95268a1ebb7071315830",
+    "url": "/react-tutorial/static/css/main.cc7ff592.chunk.css"
+  },
+  {
+    "revision": "04ae7b92f3267f0b4b02fac5ab8b95b0",
+    "url": "/react-tutorial/index.html"
+  }
+];
